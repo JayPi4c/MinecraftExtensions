31f12efeaba74a8a61b0f6dd6ede8044a221a1b4
JayPi4c <jonas.jp4c@gmail.com>
2018-10-10 22:59:38 +0200

    Der Rest des Adiomantium Crafting Tables
    Die .gitignore hatte kleine Probleme, neue Packages und Files mit zu
    adden, sodass sie in einem zweiten Commit hochgeladen werden müssen.

---
+++ src/main/java/com/JayPi4c/extensionmod/crafting/AdiomantiumCraftingTableRecipeSorter.java
@@ -0,0 +1,38 @@
+package com.JayPi4c.extensionmod.crafting;
+
+import java.util.Comparator;
+
+import net.minecraft.item.crafting.IRecipe;
+
+@SuppressWarnings("rawtypes")
+public class AdiomantiumCraftingTableRecipeSorter implements Comparator {
+
+	final AdiomantiumCraftingTableCraftingManager adiomantium_crafting_table;
+
+	public AdiomantiumCraftingTableRecipeSorter(AdiomantiumCraftingTableCraftingManager cm) {
+		this.adiomantium_crafting_table = cm;
+	}
+
+	public int compareRecipes(IRecipe recipe1, IRecipe recipe2) {
+		if (recipe1 instanceof AdiomantiumCraftingTableShapelessRecipes
+				&& recipe2 instanceof AdiomantiumCraftingTableShapedRecipes)
+			return 1;
+		else if (recipe2 instanceof AdiomantiumCraftingTableShapelessRecipes
+				&& recipe1 instanceof AdiomantiumCraftingTableShapedRecipes)
+			return -1;
+		else if (recipe2.getRecipeSize() < recipe1.getRecipeSize())
+			return -1;
+		else if (recipe2.getRecipeSize() > recipe1.getRecipeSize())
+			return 1;
+		return 0;
+
+		// return recipe1 instanceof AdiomantiumCraftingTableShapelessRecipe && recipe2
+		// instanceof AdiomantiumCraftingTableShapedRecipe ? 1
+	}
+
+	@Override
+	public int compare(Object obj1, Object obj2) {
+		return compareRecipes((IRecipe) obj1, (IRecipe) obj2);
+	}
+
+}
---
+++ src/main/java/com/JayPi4c/extensionmod/handler/GuiHandler.java
@@ -0,0 +1,33 @@
+package com.JayPi4c.extensionmod.handler;
+
+import com.JayPi4c.extensionmod.blocks.apparatuses.Apparatuses;
+import com.JayPi4c.extensionmod.container.ContainerAdiomantiumCraftingTable;
+import com.JayPi4c.extensionmod.gui.GuiAdiomantiumCraftingTable;
+
+import cpw.mods.fml.common.network.IGuiHandler;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+public class GuiHandler implements IGuiHandler {
+
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		if (ID == Apparatuses.ADIOMANTIUM_CRAFTING_TABLE_GUI_ID)
+			return world.getBlock(x, y, z) == Apparatuses.adiomantium_crafting_table
+					? new ContainerAdiomantiumCraftingTable(player.inventory, world, x, y, z)
+					: null;
+
+		return null;
+	}
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		if (ID == Apparatuses.ADIOMANTIUM_CRAFTING_TABLE_GUI_ID)
+			return world.getBlock(x, y, z) == Apparatuses.adiomantium_crafting_table
+					? new GuiAdiomantiumCraftingTable(player.inventory, world, x, y, z)
+					: null;
+		return null;
+
+	}
+
+}
