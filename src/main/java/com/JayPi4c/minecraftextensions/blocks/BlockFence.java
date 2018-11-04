4f356a735b8974912092036bd91564400a6fc06f
JayPi4c <jonas.jp4c@gmail.com>
2018-11-04 10:33:53 +0100

    Hinzufügen eines FenceBlocks
    Der FenceBlock soll den normalen Fence auf die Weise erstezen, als dass
    keine Items mehr in den Fence glitchen und somit nicht mehr an einer
    richtigen Stelle sind, dass sie gut gefarmt werden können.

---
+++ src/main/java/com/JayPi4c/minecraftextensions/blocks/BlockFence.java
@@ -0,0 +1,27 @@
+package com.JayPi4c.minecraftextensions.blocks;
+
+import java.util.List;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+
+public class BlockFence extends BlockBlock {
+
+	// TODO: Adding Texture for FenceLeaves
+	public BlockFence(String name, String texturename, SoundType stepsound, float lightlevel, float hardness,
+			Material material, String tooltype, int level) {
+		super(name, texturename, stepsound, lightlevel, hardness, material, tooltype, level);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB mask, List list,
+			Entity colliding_entity) {
+		this.setBlockBounds(0F, 0F, 0F, 1F, 1.3F, 1F);
+		super.addCollisionBoxesToList(world, x, y, z, mask, list, colliding_entity);
+		this.setBlockBounds(0F, 0F, 0F, 1F, 1.0F, 1F);
+	}
+
+}
--- src/main/java/com/JayPi4c/minecraftextensions/blocks/Blocks.java
+++ src/main/java/com/JayPi4c/minecraftextensions/blocks/Blocks.java
@@ -7,9 +7,12 @@ import net.minecraft.block.material.Material;
 public class Blocks {
 	public static Block adiomantium_block = new BlockBlock("adiomantium_block", "adiomantium_block",
 			Block.soundTypeMetal, 0F, 5F, Material.iron, "pickaxe", 1);
+	public static Block leaves = new BlockFence("leaves", "leaves", Block.soundTypeGrass, 0F, 0.8F, Material.leaves,
+			"shears", 0);
 
 	public static void init() {
 		registerBlocks(adiomantium_block, adiomantium_block.getUnlocalizedName());
+		registerBlocks(leaves, leaves.getUnlocalizedName());
 	}
 
 	public static void registerBlocks(Block block, String BlockName) {
