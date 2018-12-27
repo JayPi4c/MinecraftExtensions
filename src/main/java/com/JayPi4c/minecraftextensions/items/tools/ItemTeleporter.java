b8d0040fbe5ded8dfc7a92d6e1c1362567119d3c
JayPi4c <jonas.jp4c@gmail.com>
2018-12-27 18:36:26 +0100

    Testing on Teleporter
    Für alle Teleporter wird immer die selbe Position gespeichert, sodass
    nur einer effektiv nutzbar ist. Das muss noch behoben werden.

--- src/main/java/com/JayPi4c/minecraftextensions/items/tools/ItemTeleporter.java
+++ src/main/java/com/JayPi4c/minecraftextensions/items/tools/ItemTeleporter.java
@@ -6,12 +6,13 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.world.World;
 
 public class ItemTeleporter extends ItemItem {
 	// the position stored in the teleporter
-	double x, y, z;
+	NBTTagCompound data;
 
 	protected ItemTeleporter(String name, String texturename, int stacksize) {
 		super(name, texturename, stacksize);
@@ -19,10 +20,14 @@ public class ItemTeleporter extends ItemItem {
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
-		this.x = player.posX;
-		this.y = player.posY;
-		this.z = player.posZ;
-		return super.onItemRightClick(itemStack, world, player);
+		if (!world.isRemote) {
+			data = new NBTTagCompound();
+			data.setDouble("x-Pos", player.posX);
+			data.setDouble("y-Pos", player.posY);
+			data.setDouble("z-Pos", player.posZ);
+		}
+		return itemStack;
+
 	}
 
 	/**
@@ -36,9 +41,14 @@ public class ItemTeleporter extends ItemItem {
 
 	@Override
 	public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack) {
-		entityLiving.setPositionAndRotation(x, y, z, entityLiving.getRotationYawHead(), 0);
-		((EntityPlayer) entityLiving).addChatComponentMessage(new ChatComponentTranslation(
-				"Player " + entityLiving.getCommandSenderName() + " ported!", new Object[0]));
+		if (data != null) {
+			entityLiving.setPositionAndRotation(data.getDouble("x-Pos"), data.getDouble("y-Pos"),
+					data.getDouble("z-Pos"), entityLiving.getRotationYawHead(), 0);
+			((EntityPlayer) entityLiving).addChatComponentMessage(new ChatComponentTranslation(
+					"Player " + entityLiving.getCommandSenderName() + " ported!", new Object[0]));
+		} else
+			((EntityPlayer) entityLiving)
+					.addChatComponentMessage(new ChatComponentTranslation("No Location stored", new Object[0]));
 		return true;
 	}
 
