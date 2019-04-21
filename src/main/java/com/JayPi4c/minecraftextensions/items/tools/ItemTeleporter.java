204204712874fcc62ce670baa1e2346b1eba119b
JayPi4c <jonas.jp4c@gmail.com>
2019-04-21 19:40:12 +0200

    Minor Teleporter additions
    Es wurde dem Teleporter eine Beschreibung hinzugefügt, die die
    gespeicherte Position anzeigt. Evtl kann dafür noch ein weiteres Item
    erstellt werden, beispielsweise ein Teleporter mit Papier, auf dem die
    Daten dann geschrieben stehen würden, was das ganze etwas realistischer
    erscheinen lassen würde. Das wird man mal später genauer betrachten
    müssen.

--- src/main/java/com/JayPi4c/minecraftextensions/items/tools/ItemTeleporter.java
+++ src/main/java/com/JayPi4c/minecraftextensions/items/tools/ItemTeleporter.java
@@ -1,8 +1,11 @@
 package com.JayPi4c.minecraftextensions.items.tools;
 
+import java.util.List;
+
 import com.JayPi4c.minecraftextensions.items.ItemItem;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -18,18 +21,42 @@ public class ItemTeleporter extends ItemItem {
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
-		if (!world.isRemote) {
+		NBTTagCompound data = itemStack.getTagCompound();
+		if (data == null)
+			data = new NBTTagCompound();
 
-			NBTTagCompound data = itemStack.getTagCompound();
-			if (data == null)
-				data = new NBTTagCompound();
+		if (GuiScreen.isShiftKeyDown()) {
+			data.setString("UserInfo", "No further information stored.");
+		} else {
 			data.setDouble("x-Pos", player.posX);
 			data.setDouble("y-Pos", player.posY);
 			data.setDouble("z-Pos", player.posZ);
-			itemStack.setTagCompound(data);
 		}
+		itemStack.setTagCompound(data);
 		return itemStack;
+	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean p_77624_4_) {
+		NBTTagCompound data = itemStack.getTagCompound();
+		if (data == null)
+			list.add("No Location stored!");
+
+		else
+			list.add("Location stored: (" + Math.round(data.getDouble("x-Pos") * 10) / 10.0 + "|"
+					+ Math.round(data.getDouble("y-Pos") * 10) / 10.0 + "|"
+					+ Math.round(data.getDouble("z-Pos") * 10) / 10.0 + ")");
+		list.add("");
+		if (!GuiScreen.isShiftKeyDown())
+			list.add("Press shift for further information.");
+		else {
+			if (data != null) {
+				if (data.hasKey("UserInfo"))
+					list.add(data.getString("UserInfo"));
+			} else
+				list.add("No further information stored.");
+		}
 	}
 
 	/**
@@ -44,7 +71,7 @@ public class ItemTeleporter extends ItemItem {
 	@Override
 	public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack) {
 		NBTTagCompound data = stack.getTagCompound();
-		if (data != null) {
+		if (data.hasKey("x-Pos")) {
 			entityLiving.setPositionAndRotation(data.getDouble("x-Pos"), data.getDouble("y-Pos"),
 					data.getDouble("z-Pos"), entityLiving.getRotationYawHead(), 0);
 			((EntityPlayer) entityLiving).addChatComponentMessage(new ChatComponentTranslation(
