a67d5fea1e0a2776cb9957c0d64890b0c5853895
JayPi4c <jonas.jp4c@gmail.com>
2019-04-30 19:03:47 +0200

    Fixing Handlers
    In der Mod kann nur ein Guihandler geladen werden. Da zuvor zwei Handler
    geladen wurden wurde der AdiomantiumCraftingTable nicht mehr gerendert.
    Dies wurde jetzt gefixt.

--- src/main/java/com/JayPi4c/minecraftextensions/handler/Handlers.java
+++ src/main/java/com/JayPi4c/minecraftextensions/handler/Handlers.java
@@ -1,13 +1,39 @@
 package com.JayPi4c.minecraftextensions.handler;
 
 import com.JayPi4c.minecraftextensions.MinecraftExtensions;
+import com.JayPi4c.minecraftextensions.blocks.apparatuses.Apparatuses;
+import com.JayPi4c.minecraftextensions.container.ContainerAdiomantiumCraftingTable;
+import com.JayPi4c.minecraftextensions.gui.GuiAdiomantiumCraftingTable;
+import com.JayPi4c.minecraftextensions.gui.GuiTeleporterInput;
 
+import cpw.mods.fml.common.network.IGuiHandler;
 import cpw.mods.fml.common.network.NetworkRegistry;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 
-public class Handlers {
+public class Handlers implements IGuiHandler {
 	public static void init() {
-		NetworkRegistry.INSTANCE.registerGuiHandler(MinecraftExtensions.instance,
-				new GuiAdiomantiumCraftingTableHandler());
-		NetworkRegistry.INSTANCE.registerGuiHandler(MinecraftExtensions.instance, new GuiTeleporterHandler());
+		NetworkRegistry.INSTANCE.registerGuiHandler(MinecraftExtensions.instance, new Handlers());
+	}
+
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		if (ID == GuiAdiomantiumCraftingTable.ADIOMANTIUM_CRAFTING_TABLE_GUI_ID)
+			return world.getBlock(x, y, z) == Apparatuses.adiomantium_crafting_table
+					? new ContainerAdiomantiumCraftingTable(player.inventory, world, x, y, z)
+					: null;
+		return null;
+	}
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		if (ID == GuiTeleporterInput.TELEPORTER_GUI_ID)
+			return new GuiTeleporterInput();
+		else if (ID == GuiAdiomantiumCraftingTable.ADIOMANTIUM_CRAFTING_TABLE_GUI_ID)
+			return world.getBlock(x, y, z) == Apparatuses.adiomantium_crafting_table
+					? new GuiAdiomantiumCraftingTable(player.inventory, world, x, y, z)
+					: null;
+
+		return null;
 	}
 }
